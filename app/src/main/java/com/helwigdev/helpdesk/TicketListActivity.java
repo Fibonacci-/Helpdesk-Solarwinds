b36e6efd9b81d6f8e98245a7c4598cfa79d7fffd
Tyler <helwigdev@gmail.com>
2016-03-24 11:11:34 -0400

    Add menu activation frame

--- app/src/main/java/com/helwigdev/helpdesk/TicketListActivity.java
+++ app/src/main/java/com/helwigdev/helpdesk/TicketListActivity.java
@@ -7,10 +7,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.text.method.CharacterPickerDialog;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by helwig on 10/14/2015.
@@ -26,6 +28,22 @@ public class TicketListActivity extends SingleFragmentActivity implements Ticket
     @Override
     protected Fragment createFragment() {
         return new TicketListFragment();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_search:
+                //todo search things
+                Toast.makeText(TicketListActivity.this, "Searching for something la de da", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_settings:
+                //todo settings things
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 
     @Override
